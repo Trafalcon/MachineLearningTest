e04ac65daac1bae7911a046a893b2424eea0bdf0
Trafalcon <xdavis57@gmail.com>
2019-06-08 15:03:26 -0500

    Copied Files over

---
+++ MachineLearningMain.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using TensorFlow;
+
+namespace MachineLearningTest
+{
+    class MachineLearningMain
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
