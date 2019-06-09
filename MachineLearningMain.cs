73d7a3921fd17a1fba1cc218b8fc96db80cbd926
Aaron Fuller <afuller6@atu.edu>
2019-06-09 11:13:36 -0500

    Finished the functions given in the tutorial and tested wether or not it can compile. Currently experiencing a bug where the program cannot find the "taxi-fare-train.csv" file.

--- MachineLearningMain.cs
+++ MachineLearningMain.cs
@@ -17,6 +17,7 @@ namespace MachineLearningTest
             MLContext mlContext = new MLContext(seed: 0);
             var model = Train(mlContext, _trainDataPath);
             Evaluate(mlContext, model);
+            TestSinglePrediction(mlContext, model);
         }
 
         public static ITransformer Train(MLContext mlContext, string dataPath)
@@ -35,6 +36,34 @@ namespace MachineLearningTest
         private static void Evaluate(MLContext mlContext, ITransformer model)
         {
             IDataView dataView = mlContext.Data.LoadFromTextFile<TaxiTrip>(_testDataPath, hasHeader: true, separatorChar: ',');
+            var predictions = model.Transform(dataView);
+            var metrics = mlContext.Regression.Evaluate(predictions, "Label", "Score");
+            Console.WriteLine();
+            Console.WriteLine($"*************************************************");
+            Console.WriteLine($"*       Model quality metrics evaluation         ");
+            Console.WriteLine($"*------------------------------------------------");
+            Console.WriteLine($"*       RSquared Score:      {metrics.RSquared:0.##}");
+            Console.WriteLine($"*       Root Mean Squared Error:      {metrics.RootMeanSquaredError:#.##}");
+        }
+
+        private static void TestSinglePrediction(MLContext mLContext, ITransformer model)
+        {
+            var predictionFunction = mLContext.Model.CreatePredictionEngine<TaxiTrip, TaxiTripFarePrediction>(model);
+            var taxiTripSample = new TaxiTrip() //Test case, can be changed later
+            {
+                VendorId = "VTS",
+                RateCode = "1",
+                PassengerCount = 1,
+                TripTime = 1140,
+                TripDistance = 3.75f,
+                PaymentType = "CRD",
+                FareAmount = 0 // To predict. Actual/Observed = 15.5
+            };
+            var prediction = predictionFunction.Predict(taxiTripSample);
+
+            Console.WriteLine($"**********************************************************************");
+            Console.WriteLine($"Predicted fare: {prediction.FareAmount:0.####}, actual fare: 15.5");
+            Console.WriteLine($"**********************************************************************");
         }
     }
 
