62047c1d02b102d8ed48e025ec719f1156553e86
Trafalcon <xdavis57@gmail.com>
2019-06-08 19:02:58 -0500

    Fit Columns to Data Set

--- MachineLearningMain.cs
+++ MachineLearningMain.cs
@@ -24,19 +24,34 @@ namespace MachineLearningTest
         }
     }
 
-     class SalaryData
+     class TaxiTrip
     {
         [LoadColumn(0)]
-        public float YearsExperience;
+        public string VendorId;
 
         [LoadColumn(1)]
-        public float Salary;
+        public string RateCode;
+
+        [LoadColumn(2)]
+        public float PassengerCount;
+
+        [LoadColumn(3)]
+        public float TripTime;
+
+        [LoadColumn(4)]
+        public float TripDistance;
+
+        [LoadColumn(5)]
+        public string PaymentType;
+
+        [LoadColumn(6)]
+        public float FareAmount;
     }
 
-     class SalaryDataPrediction
+     class TaxiTripFarePrediction
     {
         [ColumnName("Score")]
-        public float Salary;
+        public float FareAmount;
     }
         
 }
